d8bca54f56ac82c1254eb0f364ba7ef935529c87
Scarlet Coder <88984475+ScarletCoder7@users.noreply.github.com>
2022-11-11 20:59:39 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 $(function(){
     
     //Same as document.querySelector("#navbarToggle").addEventListener("blur", function(event){});
-    $("#navbarToggle").blur(function(event){    //.blur() to go back to the original form when looses focus !! // so if .blur() happens the below code will be executed !!
-        var screenWidth = window.innerWidth; // window.innerWidth means width of the browser window
+    $("#navbarToggle").blur(function(event){    
+        var screenWidth = window.innerWidth; 
         if(screenWidth < 728){
             $("#collapsable-nav").collapse("hide");
         }
